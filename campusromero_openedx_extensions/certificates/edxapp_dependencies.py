2d383332f17a59b04f0ae8380ffe94f545b28975
Squirrel18 <daniel.quiroga@edunext.co>
2018-10-23 17:22:38 -0500

    Add change course mode function.
    Change course mode.
    
    Change edx-platform dependencies imports.
    
    Clean no longer used settings.
    
    Change edx dependencies file location.

---
+++ campusromero_openedx_extensions/certificates/edxapp_dependencies.py
@@ -0,0 +1,6 @@
+"""
+Module to import edx platform dependencies.
+"""
+from course_modes.models import CourseMode
+from lms.djangoapps.certificates.models import CertificateWhitelist
+from student.models import CourseEnrollment
