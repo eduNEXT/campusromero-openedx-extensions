8b20420c351abb8dbd63354f979275f1115c923b
Luis Moreno <luis.moreno@edunext.co>
2019-11-04 13:27:13 -0400

    First try

---
+++ campusromero_openedx_extensions/general_custom_views/api/urls.py
@@ -0,0 +1,14 @@
+""" urls.py """
+
+from django.conf.urls import url, include
+
+
+urlpatterns = [  # pylint: disable=invalid-name
+    url(
+        r'^v1/',
+        include(
+            'campusromero_openedx_extensions.general_custom_views.api.v1.urls',
+            namespace='camrom-api'
+        )
+    ),
+]
---
+++ campusromero_openedx_extensions/general_custom_views/api/v1/views.py
@@ -0,0 +1,68 @@
+"""
+Views for the camrom api.
+"""
+import uuid
+
+from rest_framework import status
+from rest_framework import permissions
+
+from rest_framework.views import APIView
+
+from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication  # pylint: disable=import-error,line-too-long
+
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+from lms.djangoapps.verify_student.models import SoftwareSecurePhotoVerification  # pylint: disable=import-error
+
+class ChangeToVerifiedMode(APIView):
+    """
+    Change the status to 'approved' in Software Secure Photo Verification model for the given user.
+    """
+    authentication_classes = (JwtAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
+
+    def post(self, request):
+        """
+        Change the status of the user into Software Secure Photo Verification model,
+        if it not exist, is created instead with status value 'approved' by default.
+        Receive a username key, with the username value to change status.
+        POST api/user/v1/preferences/change_to_verified_mode/
+        Returns:
+            Json object that contains two keys: status and username.
+            - status: Inidicate the operation status, created, updated or user does not exist error.
+            - username: Contains the username provided in post request.
+        """
+        if not request.data.get('username'):
+            return JsonResponse({
+                'status': 'No username key provided.',
+                'username': ''
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        username = request.data['username']
+        operation_status = 'updated'
+        try:
+            user_info = User.objects.get(username=username)  # pylint: disable=no-member
+        except User.DoesNotExist:  # pylint: disable=no-member
+            return JsonResponse({
+                'status': 'The user with username: {}, does not exist.'.format(username),
+                'username': username
+            }, status=status.HTTP_404_NOT_FOUND)
+        try:
+            user_photo_verification = SoftwareSecurePhotoVerification.objects.get(
+                user_id=user_info.id)
+            user_photo_verification.status = 'approved'
+            user_photo_verification.save()
+        except SoftwareSecurePhotoVerification.DoesNotExist:
+            operation_status = 'created'
+            receipt_id = str(uuid.uuid4())
+            user_photo_verification = SoftwareSecurePhotoVerification(
+                status='approved',
+                receipt_id=receipt_id,
+                user_id=user_info.id
+            )
+            user_photo_verification.save()
+        return JsonResponse({
+            'status': operation_status,
+            'username': username
+        }, status=status.HTTP_200_OK)
